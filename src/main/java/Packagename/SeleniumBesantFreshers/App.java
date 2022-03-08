99dc9dfb0596cea407bbcada93d6953f7db3532f
sathish1980 <kumar.sathish189@gmail.com>
2022-03-08 09:17:43 +0530

    new project

---
+++ src/main/java/Packagename/SeleniumBesantFreshers/App.java
@@ -0,0 +1,13 @@
+package Packagename.SeleniumBesantFreshers;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
