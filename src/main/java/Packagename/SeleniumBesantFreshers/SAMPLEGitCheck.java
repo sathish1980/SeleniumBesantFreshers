605c4797cf2ea0e3ecb721bc4a9b8134c2c11401
sathish1980 <kumar.sathish189@gmail.com>
2022-03-08 09:22:15 +0530

    New sample file added

---
+++ src/main/java/Packagename/SeleniumBesantFreshers/SAMPLEGitCheck.java
@@ -0,0 +1,15 @@
+package Packagename.SeleniumBesantFreshers;
+
+public class SAMPLEGitCheck {
+	
+	public void add() {
+		System.out.println("Additon");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SAMPLEGitCheck s = new SAMPLEGitCheck();
+		s.add();
+	}
+
+}
